6a3eb5c729d72d1829897eab8e3d314c8a93a2b2
Dennis Griffin <dgdev1024@gmail.com>
2022-05-14 15:27:59 +0000

    Added CORS...

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@
 
 const path = require("path");
 const express = require("express");
+const cors = require('cors');
 
 // Initialize app...
 const app = express();
+
+// Middleware
+app.use(cors())
 
 // Serve public folder...
 app.use("/public", express.static(path.join(__dirname, "public")));
